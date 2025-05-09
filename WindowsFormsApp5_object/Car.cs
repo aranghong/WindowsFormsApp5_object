24b7247ca8da52bc12ee138182be164f8cbec856
aranghong <har2129@yu.ac.kr>
2025-05-09 13:45:00 +0900

    Add project files.

---
+++ WindowsFormsApp5_object/Car.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5_object
+{
+    internal class Car
+    {
+        private int _speed;
+        private string _modelName;
+        private int year;
+        private readonly string manufacturer = "현대";
+
+        public static int totalCar = 0;
+
+        public int speed
+        {
+            get { return _speed; }
+            set
+            {
+                if (value < 0) _speed = 0;   //음수 방지
+                else _speed = value;
+            }
+        }
+
+        public string color { get; set; }
+
+        public string modelName
+        {
+            get { return _modelName; }
+            private set { _modelName = value; }
+        }
+
+
+        public bool IsNewCar
+        {
+            get { return year >= 2023; }
+        }
+
+
+        public string summary
+        {
+            get { return $"{year}년식 {manufacturer} {modelName} - {color} - {speed} \r\n"; }
+        }
+
+        //생성자
+        public Car()
+        {
+            modelName = "이름 없는 차";
+            speed = 0;
+            color = "회색";
+            year = 2000;
+            totalCar++;
+
+            Console.WriteLine("기본 생성자 car 생성");
+
+        }
+
+        public Car(string model, int speed, string color, int year)
+        {
+            modelName = model;
+            _speed = speed;
+            this.color = color;
+            this.year = year;
+            totalCar++;
+
+            Console.WriteLine($"{model} 객체 생성");
+        }
+
+        //소멸자
+        ~Car()
+        {
+            Console.WriteLine($"{modelName} 객체가 메모리에서 삭제");
+        }
+
+        //메서드
+        public void accel(int amount)
+        {
+            speed += amount;
+            Console.WriteLine("속도 증가");
+        }
+        public void brake(int amount)
+        {
+            speed -= amount;
+            if (speed < 0) speed = 0;
+            Console.WriteLine("속도 감소");
+        }
+
+        public void print()
+        {
+            Console.WriteLine("-----차량 정보-------");
+            Console.WriteLine($"제조사: {manufacturer}");
+            Console.WriteLine($"모델명: {modelName}");
+            Console.WriteLine($"색상: {color}");
+            Console.WriteLine($"속도: {speed}");
+            Console.WriteLine($"생산년도: {year}");
+            Console.WriteLine($"신차여부: {(IsNewCar ? "신차!!" : "구형 차량")}");
+            Console.WriteLine($"요약: {summary}");
+            Console.WriteLine($"총 생산 차량 수: {totalCar}");
+        }
+    }
+}
---
+++ WindowsFormsApp5_object/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5_object
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //생성자
+            Car car1 = new Car();
+            car1.speed = 20;
+            car1.color = "은색";
+            car1.print();
+
+
+            Console.WriteLine("\r\n-----------------------\r\n");
+
+            Car car2 = new Car("그랜저", 50, "검정", 2024);
+            car2.print();
+            car2.accel(30);
+            car2.print();
+
+
+
+        }
+    }
+
+    
+}
